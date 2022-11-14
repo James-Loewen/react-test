20f55b8d35f784732a8d0852a963961bceb6c13f
James Loewen <jamesfloewen@gmail.com>
2022-11-13 21:31:24 -0500

    Adapt Library site to React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './main.scss';
+
+const myLibrary = [
+  {title: 'The Hobbit', author: 'J.R.R. Tolkien'},
+  {title: 'The Giver', author: 'Lois Lowry'},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {myLibrary.foreach(book => {
+        <Book title={book.title} author={book.author} />
+      })}
     </div>
   );
 }
 
+class Book extends React.Component {
+  render() {
+    return (
+      <div className='card'>
+        <span className='title'>{this.props.title}</span>
+        <span className='author'>{this.props.author}</span>
+        <Button class='read-status read' text='Read' />
+        <Button class='delete-book' text='delete' />
+      </div>
+    );
+  }
+}
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className={this.props.class}>{this.props.text}</button>
+    );
+  }
+}
+
+// export default App;
 export default App;
